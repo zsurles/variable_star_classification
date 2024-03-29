e33eea2161661885a7f5fd6e9666e30d9ed9d7a3
Kirill Sokolovsky <kirx007@gmail.com>
2024-03-29 22:16:25 +0000

    add end-of-line ancor to the grep for asassnid in update_combined_txt.sh and correct output file name typo

--- downloaded_lightcurves/update_combined_txt.sh
+++ downloaded_lightcurves/update_combined_txt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in *_classification.txt ;do 
  grep 'class = RR' $i && continue 
- grep "asassn_id = $(basename $i _classification.txt)" combined.txt && continue 
- cat $i >> cmbined.txt 
+ grep "asassn_id = $(basename $i _classification.txt)"'$' combined.txt && continue 
+ cat $i >> combined.txt 
 done
