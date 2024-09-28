ff23651db9c31d5cc288f740aea7bd07303f3892
Kirill Sokolovsky <kirx@kirx.net>
2024-09-27 19:32:01 -0500

    get_dust.py with all the standard bands

--- lightcurves/get_dust.py
+++ lightcurves/get_dust.py
@@ -21,9 +21,24 @@ def main():
         print("Invalid input. RA, Dec, and Distance must be numbers.")
         sys.exit(1)
 
+    # Load g band extinction map
+    combined19_g= mwdust.Combined19(filter='Gunn g')
+    # Load g band extinction map
+    combined19_r= mwdust.Combined19(filter='Gunn r')
+    # Load g band extinction map
+    combined19_i= mwdust.Combined19(filter='Gunn i')
+    # Load B band extinction map
+    combined19_B= mwdust.Combined19(filter='Landolt B')
+    # Load V band extinction map
+    combined19_V= mwdust.Combined19(filter='Landolt V')
+    # Load R band extinction map
+    combined19_R= mwdust.Combined19(filter='Landolt R')
+    # Load I band extinction map
+    combined19_I= mwdust.Combined19(filter='Landolt I')    
     # Load J band extinction map
-    combined19_J= mwdust.Combined19(filter='2MASS J')
-    
+    combined19_J= mwdust.Combined19(filter='2MASS J')    
+    # Load H band extinction map
+    combined19_H= mwdust.Combined19(filter='2MASS H')
     # Load K band extinction map
     combined19_K= mwdust.Combined19(filter='2MASS Ks')
     
@@ -33,18 +48,47 @@ def main():
     # Convert to galactic coordinates
     galactic_coords = equatorial_coords.galactic
     
+    # Compute g band extinction
+    combined19_g_result= combined19_g(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute r band extinction
+    combined19_r_result= combined19_r(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute i band extinction
+    combined19_i_result= combined19_i(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute B band extinction
+    combined19_B_result= combined19_B(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute V band extinction
+    combined19_V_result= combined19_V(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute R band extinction
+    combined19_R_result= combined19_R(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
+    # Compute I band extinction
+    combined19_I_result= combined19_I(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
     # Compute J band extinction
     combined19_J_result= combined19_J(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
-
+    # Compute H band extinction
+    combined19_H_result= combined19_H(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
     # Compute K band extinction
     combined19_K_result= combined19_K(equatorial_coords.galactic.l.value,equatorial_coords.galactic.b.value,galactic_coords.distance.value/1000)
 
     # Print the result
+    print("Input parameters:")
     print(f"Galactic Longitude (l): {galactic_coords.l:.6f}")
     print(f"Galactic Latitude (b): {galactic_coords.b:.6f}")
     print(f"Distance: {galactic_coords.distance:.6f}")
-    print(f"J band extinction at: {combined19_J_result[0]:.3f}")
-    print(f"K band extinction at: {combined19_K_result[0]:.3f}")
+    print("---------------------------------------")
+    print("Extinction from mwdust.Combined19:")
+    print("---------------------------------------")
+    print(f"g band extinction: {combined19_g_result[0]:.3f} mag")
+    print(f"r band extinction: {combined19_r_result[0]:.3f} mag")
+    print(f"i band extinction: {combined19_i_result[0]:.3f} mag")
+    print("---------------------------------------")
+    print(f"B band extinction: {combined19_B_result[0]:.3f} mag")
+    print(f"V band extinction: {combined19_V_result[0]:.3f} mag")
+    print(f"R band extinction: {combined19_R_result[0]:.3f} mag")
+    print(f"I band extinction: {combined19_I_result[0]:.3f} mag")
+    print("---------------------------------------")
+    print(f"J band extinction: {combined19_J_result[0]:.3f} mag")
+    print(f"H band extinction: {combined19_H_result[0]:.3f} mag")
+    print(f"K band extinction: {combined19_K_result[0]:.3f} mag")
 
 if __name__ == "__main__":
     main()
