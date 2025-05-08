7b48c0b405b9f5f1bff904bdb439b3b388a013b1
Kirill Sokolovsky <kirx@kirx.net>
2025-05-08 14:13:19 -0500

    update asassn_id_only.txt and asassn_id_only.txt

--- downloaded_lightcurves/asassn_id.sh
+++ downloaded_lightcurves/asassn_id.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-for i in *_classification.txt; do 
-    if grep -q 'class = RR' "$i"; then                                                     
-        asassn_id=$(basename "$i" _classification.txt)
-        if ! grep -q "asassn_id = $asassn_id" RRconfirmed.txt; then
-            echo "$asassn_id" >> asassn_id_only.txt
-        fi
-    fi
-done
+#for i in *_classification.txt; do 
+#    if grep -q 'class = RR' "$i"; then                                                     
+#        asassn_id=$(basename "$i" _classification.txt)
+#        if ! grep -q "asassn_id = $asassn_id" RRconfirmed.txt; then
+#            echo "$asassn_id" >> asassn_id_only.txt
+#        fi
+#    fi
+#done
+
+# Select $asassn_id of RR Lyrae stars
+if [ ! -f ../lightcurves/distance_color_type_for_all_stars.txt ];then
+ echo "../lightcurves/distance_color_type_for_all_stars.txt  -- classification file not found"
+ exit 1
+fi
+
+grep 'FinalType= RR' ../lightcurves/distance_color_type_for_all_stars.txt | awk '{print $2}' > asassn_id_only.txt
 
 # need to figure out a way to continue to add to the asassn_id_only.txt without duplicating stars that have already been added to the file
