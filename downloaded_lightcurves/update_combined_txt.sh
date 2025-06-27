1a1993569830027524a3fbc4d3be6f7aa73f1f4d
Kirill Sokolovsky <kirx007@gmail.com>
2025-06-27 07:04:09 +0000

    non_RR_objects_inside_rr_region in combined.txt

--- downloaded_lightcurves/update_combined_txt.sh
+++ downloaded_lightcurves/update_combined_txt.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 for i in *_classification.txt ;do 
- grep 'class = RR' $i && continue 
- grep "asassn_id = $(basename $i _classification.txt)"'$' combined.txt && continue 
- cat $i >> combined.txt 
+ ADD_THIS_STAR_TO_COMBINED_TXT=1
+ ASASSNID=$(basename $i _classification.txt)
+ if grep --quiet 'class = RR' $i ;then
+  ADD_THIS_STAR_TO_COMBINED_TXT=0
+ fi
+ if grep --quiet "^$ASASSNID," ../lightcurves/RR_objects_outside_rr_region.csv ;then
+  ADD_THIS_STAR_TO_COMBINED_TXT=1
+ fi
+ if grep --quiet "asassn_id = $ASASSNID"'$' combined.txt ;then
+  ADD_THIS_STAR_TO_COMBINED_TXT=0
+ fi
+ if [ $ADD_THIS_STAR_TO_COMBINED_TXT -eq 1 ];then
+  cat $i >> combined.txt 
+  if ! grep --quiet 'visual_inspection_comments' $i ;then
+   echo "color_string =
+visual_inspection_comments =" >> combined.txt
+  fi
+ fi
 done
